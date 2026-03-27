7d3105972828a46ec5c70cac34ab52924e23f1a3
E1isey <e.lukashchuk24@mail.ru>
2026-03-27 11:42:41 +0300

    Added basic UI and functionality for adding tasks

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Project scripts
+function addTask() {
+  const input = document.getElementById("newTaskInput");
+
+  if (input.value.trim()) {
+    const taskList = document.getElementById("taskList");
+    const newItem = document.createElement("li");
+    newItem.textContent = input.value.trim();
+    taskList.appendChild(newItem);
+    input.value = "";
+  }
+}
